02fedebacef3b0ef3ec0e6d615e8032f90de37b8
joe-mgl <otepmigue@gmail.com>
2020-10-22 11:22:16 -0700

    Updates

---
+++ precache-manifest.c07ca34dc4957d78be471fb22aa8c472.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e836a32ff6272e0c51aa73bae9923ae4",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "41b302d0fc6bb8ebe540",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a4cd015bc260d00d9dbb",
+    "url": "/robofriends/static/css/main.2a6ac75b.chunk.css"
+  },
+  {
+    "revision": "41b302d0fc6bb8ebe540",
+    "url": "/robofriends/static/js/2.d25fa9f7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/robofriends/static/js/2.d25fa9f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4cd015bc260d00d9dbb",
+    "url": "/robofriends/static/js/main.501951fa.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  },
+  {
+    "revision": "6b9fcce45df31f16296c48fc42b49fec",
+    "url": "/robofriends/static/media/TransformersMovie-y9Ad.6b9fcce4.woff"
+  }
+]);
